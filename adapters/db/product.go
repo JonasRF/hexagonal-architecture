e3f1c27a90a07b7405271949e391fdf46ad95830
JonasRF <jonasribeiro41@yahoo.com.br>
2024-07-22 23:53:12 -0300

    Preparing database tests and creating Get method tests

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -2,20 +2,26 @@ package db
 
 import (
 	"database/sql"
+
 	"github.com/codeedu/go-hexagonal/application"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type ProductDb struct {
 	db *sql.DB
 }
 
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db: db}
+}
+
 func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 	var product application.Product
-	stmp, err := p.db.Prepare("select id, name, price, status from products where id=?")
+	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
 	if err != nil {
 		return nil, err
 	}
-	err = stmp.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
 	if err != nil {
 		return nil, err
 	}
