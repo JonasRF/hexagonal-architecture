4bae9abb6587ef7bb595fc8e280e70757b15cf47
JonasRF <jonasribeiro41@yahoo.com.br>
2024-08-26 16:53:15 -0300

    Creating Web Server

---
+++ adapters/web/server/server.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"github.com/codeedu/go-hexagonal/application"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type WebServer struct {
+	Service application.ProductServiceInterface
+}
+
+func makeNemWebServer() *WebServer {
+	return &WebServer{}
+}
+
+func (w WebServer) Serve() {
+
+	r := mux.NewRouter()
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":8080",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Llongfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
