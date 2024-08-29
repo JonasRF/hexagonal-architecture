81abc58209ea1580947980799a1233f9dab9420e
JonasRF <jonasribeiro41@yahoo.com.br>
2024-08-28 21:37:24 -0300

    Initing implementation the handlers and finishing implementation the getProduct

--- adapters/web/server/server.go
+++ adapters/web/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/codeedu/go-hexagonal/adapters/web/handler"
 	"github.com/codeedu/go-hexagonal/application"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
@@ -24,6 +25,9 @@ func (w WebServer) Serve() {
 	n := negroni.New(
 		negroni.NewLogger(),
 	)
+
+	handler.MakeProductHandler(r, n, w.Service)
+	http.Handle("/", r)
 
 	server := &http.Server{
 		ReadHeaderTimeout: 10 * time.Second,
